bcc3963228b77d558a788d4d9b7ddb56a70e9473
Ezequiel Bär Coch <eebarcoch@gmail.com>
2018-05-11 07:25:11 -0300

    Added google sign-in service to backend and logic to handle token

--- config/config.js
+++ config/config.js
@@ -1,1 +1,2 @@
 module.exports.SEED = 'h0sP1t4L';
+module.exports.CLIENT_ID = '498917783540-njftii7k27dc9j1l0fdibeubu3mmsml9.apps.googleusercontent.com';
--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,60 @@ const app = express();
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 const SEED = require("../config/config").SEED;
+const CLIENT_ID = require("../config/config").CLIENT_ID;
 const jwt = require("jsonwebtoken");
+
+
+
+// ==================================================
+//  Login with Google
+// ==================================================
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
+
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    const payload = ticket.getPayload();
+    //const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
+    return {
+        name: payload.name,
+        email: payload.email,
+        img: payload.picture
+    }
+}
+//verify().catch(console.error);
+
+
+app.post('/google', async(req, res) => {
+
+    const token = req.body.token;
+
+    let googleUser = await verify(token)
+        .catch(err => {
+            return res.status(403).json({
+                success: false,
+                notifications: 'Invalid token ' + err,
+                data: null
+            });
+        });
+
+    res.status(200).json({
+        sucess: true,
+        data: googleUser
+    });
+
+});
+
+// ==================================================
+//  Default Login
+// ==================================================
 
 app.post('/', (req, res) => {
     const body = req.body;
